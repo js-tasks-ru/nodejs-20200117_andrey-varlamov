a0746310ea8c69fabc18f689e656d7520304286e
Andrey Varlamov <54108521+drummer1992@users.noreply.github.com>
2020-01-30 15:01:54 +0200

    Update server.js

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -9,7 +9,6 @@ const fs = require('fs');
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  console.log(url.parse(req.url));
   const pathname = url.parse(req.url).pathname.slice(1);
   const filepath = path.join(__dirname, 'files', pathname);
   const fileToSend = fs.createReadStream(filepath);
