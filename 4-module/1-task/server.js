3de7adbb2c28df98707b8891fcfcdb5887311d35
drummer1992 <avarlamov82@gmail.com>
2020-01-30 14:49:19 +0200

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,17 +1,32 @@
+'use strict';
+
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
+  console.log(url.parse(req.url));
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
-
+  const fileToSend = fs.createReadStream(filepath);
+  fileToSend.on('error', (e) => {
+    if (e.code === 'ENOENT') {
+      if (path.dirname(pathname).length > 1) {
+        res.statusCode = 400;
+        res.end('статус код ответа 400');
+      }
+      res.statusCode = 404;
+      res.end('статус код ответа 404');
+    }
+  });
   switch (req.method) {
     case 'GET':
-
+      res.statusCode = 200;
+      fileToSend.pipe(res);
       break;
 
     default:
