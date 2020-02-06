0798f29f28992d48a40f29e3863bb9383e0b9d13
drummer1992 <avarlamov82@gmail.com>
2020-02-06 16:04:01 +0200

    5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -19,15 +19,22 @@ router.get('/subscribe', async (ctx, next) => {
   const message = await new Promise((resolve) => {
     subscribers[_id] = resolve;
   });
+  delete subscribers[_id];
   ctx.body = message;
 });
 
 
 router.post('/publish', async (ctx, next) => {
   const message = ctx.request.body.message;
+  if (!message) {
+    ctx.status = 200;
+    return;
+  }
   for (const id in subscribers) {
-    if (subscribers[id] && (message !== '')) {
-      subscribers[id](message);
+    if (subscribers[id]) {
+      const resolve = subscribers[id];
+      resolve(message);
+      ctx.status = 200;
     }
   }
 });
