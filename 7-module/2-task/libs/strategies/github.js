069e7d4bc6a430b20a1fd18bb9fc171181adc83e
drummer1992 <avarlamov82@gmail.com>
2020-02-13 21:27:45 +0200

    add solution for 7-module 2-task

--- 7-module/2-task/libs/strategies/github.js
+++ 7-module/2-task/libs/strategies/github.js
@@ -10,7 +10,6 @@ module.exports = new GithubStrategy({
   scope: ['user:email'],
   session: false,
 }, function(accessToken, refreshToken, profile, done) {
-  console.log(accessToken, profile);
   authenticate('github', get(profile, 'emails[0].value'), profile.username, done);
 }
 );
